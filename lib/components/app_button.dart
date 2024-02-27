90ec3f4f8bdf63dca37e715e99876388b8aafef3
Juwonlo <owoeyejuju@gmail.com>
2024-02-27 23:56:23 +0100

    Working

--- lib/components/app_button.dart
+++ lib/components/app_button.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppButton extends StatelessWidget {
-  const AppButton({Key? key}) : super(key: key);
+
+  const AppButton({ super.key });
 
   @override
   Widget build(BuildContext context) {
