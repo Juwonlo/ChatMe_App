981407ad91bd94c98b3b167c1805454e2e63cecb
Juwonlo <owoeyejuju@gmail.com>
2024-04-01 15:30:43 +0100

    Components

--- lib/components/app_button.dart
+++ lib/components/app_button.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 
 class AppButton extends StatelessWidget {
+   final void Function()? onTap;
+   final String text;
 
-  const AppButton({ super.key });
+  const AppButton({
+    super.key,
+   required this.onTap,
+    required this.text,
+  });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: onTap,
       child: Container(
-        width: 34,
-      height: 33,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(7),
-         color: Colors.grey
+         color: Colors.black
       ),
+        child: Center(
+          child: Text(text),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chatme_app/auth/screens/login_page_screens.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      home: LoginPageScreen(),
     );
   }
 }
